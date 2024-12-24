aa0d45ed43385c21c39d7b47a2fbd1393fd9a458
twillecke <thiago.willecke@ino-games.com>
2024-12-24 14:23:50 -0300

    feat: add google analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,10 @@ import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
+import partytown from "@astrojs/partytown";
 
 export default defineConfig({
   site: "https://twillecke.github.io/",
   base: "/site/", 
-  integrations: [mdx(), sitemap(), tailwind()],
+  integrations: [mdx(), sitemap(), tailwind(), partytown({config: {forward: ['dataLayer.push']}})],
 });
---
+++ src/scripts/cookieconsent-config.js
@@ -0,0 +1,71 @@
+import 'https://cdn.jsdelivr.net/gh/orestbida/cookieconsent@3.0.1/dist/cookieconsent.umd.js';
+
+// Enable dark mode
+document.documentElement.classList.add('cc--darkmode');
+
+CookieConsent.run({
+    guiOptions: {
+        consentModal: {
+            layout: "box",
+            position: "bottom left",
+            equalWeightButtons: true,
+            flipButtons: false
+        },
+        preferencesModal: {
+            layout: "box",
+            position: "right",
+            equalWeightButtons: true,
+            flipButtons: false
+        }
+    },
+    categories: {
+        necessary: {
+            readOnly: true
+        },
+        analytics: {}
+    },
+    language: {
+        default: "en",
+        autoDetect: "browser",
+        translations: {
+            en: {
+                consentModal: {
+                    title: "Cookies",
+                    description: "This website uses cookies to analyze traffic and optimize your experience.",
+                    acceptAllBtn: "Accept all",
+                    acceptNecessaryBtn: "Reject all",
+                    showPreferencesBtn: "Manage preferences",
+                    footer: "<a href=\"#link\">Privacy Policy</a>\n<a href=\"#link\">Terms and conditions</a>"
+                },
+                preferencesModal: {
+                    title: "Consent Preferences Center",
+                    acceptAllBtn: "Accept all",
+                    acceptNecessaryBtn: "Reject all",
+                    savePreferencesBtn: "Save preferences",
+                    closeIconLabel: "Close modal",
+                    serviceCounterLabel: "Service|Services",
+                    sections: [
+                        {
+                            title: "Cookie Usage",
+                            description: "This website uses cookies to analyze traffic and optimize your experience."
+                        },
+                        {
+                            title: "Strictly Necessary Cookies <span class=\"pm__badge\">Always Enabled</span>",
+                            description: "These cookies are essential for the website to function properly.",
+                            linkedCategory: "necessary"
+                        },
+                        {
+                            title: "Analytics Cookies",
+                            description: "These cookies are used to collect information about how you use the website.",
+                            linkedCategory: "analytics"
+                        },
+                        {
+                            title: "More information",
+                            description: "For any query in relation to my policy on cookies and your choices, please <a class=\"cc__link\" href=\"#yourdomain.com\">contact me</a>."
+                        }
+                    ]
+                }
+            }
+        }
+    }
+});
